7b6201fa9d6c6df9fc6ed0f769ea5bab7fbb986a
JoVanko <1728931104@qq.com>
2024-12-10 16:26:08 +0800

    2 Basic第二节图形

---
+++ Basics/BuildingAGraph/Assets/Scripts/Graph.cs
@@ -0,0 +1,66 @@
+using System;
+using UnityEngine;
+
+public class Graph : MonoBehaviour
+{
+    [SerializeField] Transform pointPrefab;
+    [SerializeField, Range(10, 1000)] private int resolution = 10;
+    private Transform[] points;
+
+    private void Awake()
+    {
+        // for (int i = 0; i < 10; i++)
+        // {
+        //     Transform point = Instantiate(pointPrefab);
+        //     point.localPosition = Vector3.right * i / 5;
+        //     point.localPosition = Vector3.right * i;
+        // }
+
+        // for (int i = 0; i < 10; i++)
+        // {
+        //     Transform point = Instantiate(pointPrefab);
+        //     point.localPosition = Vector3.right * i;
+        //     // point.localScale = Vector3.one / 5f;
+        //     // point.localPosition = Vector3.right * (i / 5f - 1f);
+        //     point.localPosition = Vector3.right * ((i + 0.5f) / 5f - 1f);
+        // }
+
+        // Vector3 position = new Vector3();
+        // var scale = Vector3.one / 5f;
+        // for (int i = 0; i < resolution; i++)
+        // {
+        //     Transform point = Instantiate(pointPrefab);
+        //     // point.localPosition = Vector3.right * ((i + 0.5f) / 5f - 1f);
+        //     // point.localScale = scale;
+        //     position.x = (i + 0.5f) / 5f - 1f;
+        //     position.y = position.x * position.x;
+        //     point.localPosition = position;
+        //     point.localScale = scale;
+        // }
+        points = new Transform[resolution];
+        float step = 2f / resolution;
+        var position = Vector3.zero;
+        var scale = Vector3.one * step;
+        for (int i = 0; i < points.Length; i++)
+        {
+            Transform point = Instantiate(pointPrefab, transform, false);
+            points[i] = point;
+            position.x = (i + 0.5f) * step - 1f;
+            // position.y = position.x * position.x * position.x;
+            point.localPosition = position;
+            point.localScale = scale;
+        }
+    }
+
+    private void Update()
+    {
+        var time = Time.time;
+        for (int i = 0; i < points.Length; i++)
+        {
+            Transform point = points[i];
+            Vector3 position = point.localPosition;
+            position.y = Mathf.Sin(Mathf.PI * (position.x + time));
+            point.localPosition = position;
+        }
+    }
+}
