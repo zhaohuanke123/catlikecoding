512e62a79e6c655b274668cc1215232c1558bfbe
JoVanko <1728931104@qq.com>
2024-12-31 10:34:42 +0800

    1 完成 3-1 小球移动

--- Movement/SlidingASphere/Assets/Script/MovingSphere.cs
+++ Movement/SlidingASphere/Assets/Script/MovingSphere.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.Serialization;
 
 
 public class MovingSphere : MonoBehaviour
@@ -7,50 +8,52 @@ public class MovingSphere : MonoBehaviour
 
     void Update()
     {
-        // 获取输入并限制到单位长度
+        // 1. 获取输入并限制到单位长度
         var playerInput = default(Vector2);
         playerInput.x = Input.GetAxis("Horizontal");
         playerInput.y = Input.GetAxis("Vertical");
         playerInput = Vector2.ClampMagnitude(playerInput, 1f);
 
-        // Vector3 acceleration =
-        //     new Vector3(playerInput.x, 0f, playerInput.y) * maxSpeed;
-        Vector3 desiredVelocity =
-            new Vector3(playerInput.x, 0f, playerInput.y) * maxSpeed;
-        float maxSpeedChange = maxAcceleration * Time.deltaTime;
-        // if (velocity.x < desiredVelocity.x)
-        // {
-        //     // velocity.x += maxSpeedChange;
-        //     velocity.x =
-        //         Mathf.Min(velocity.x + maxSpeedChange, desiredVelocity.x);
-        // }
-        // else if (velocity.x > desiredVelocity.x)
-        // {
-        //     velocity.x =
-        //         Mathf.Max(velocity.x - maxSpeedChange, desiredVelocity.x);
-        // }
+        //  2. 获取当前输入控制的速度
+        var desiredVelocity =
+            new Vector3(playerInput.x, 0f, playerInput.y) * m_maxSpeed;
+        float maxSpeedChange = m_maxAcceleration * Time.deltaTime;
 
-        velocity.x =
-            Mathf.MoveTowards(velocity.x, desiredVelocity.x, maxSpeedChange);
-        velocity.z =
-            Mathf.MoveTowards(velocity.z, desiredVelocity.z, maxSpeedChange);
-        // velocity += acceleration * Time.deltaTime;
-        Vector3 displacement = velocity * Time.deltaTime;
+        // 3. 控制速度变化加速
+        m_velocity.x =
+            Mathf.MoveTowards(m_velocity.x, desiredVelocity.x, maxSpeedChange);
+        m_velocity.z =
+            Mathf.MoveTowards(m_velocity.z, desiredVelocity.z, maxSpeedChange);
 
-        transform.localPosition += displacement;
+        // 4. 计算位移变化量
+        var displacement = m_velocity * Time.deltaTime;
+
+        // 5. 限制移动访问, 各个方向分别判断，防止粘墙
+        var newPosition = transform.localPosition + displacement;
+        if (newPosition.x < m_allowedArea.xMin)
+        {
+            newPosition.x = m_allowedArea.xMin;
+            m_velocity.x = -m_velocity.x * m_bounciness;
+        }
+        else if (newPosition.x > m_allowedArea.xMax)
+        {
+            newPosition.x = m_allowedArea.xMax;
+            m_velocity.x = -m_velocity.x * m_bounciness;
+        }
+
+        if (newPosition.z < m_allowedArea.yMin)
+        {
+            newPosition.z = m_allowedArea.yMin;
+            m_velocity.z = -m_velocity.z * m_bounciness;
+        }
+        else if (newPosition.z > m_allowedArea.yMax)
+        {
+            newPosition.z = m_allowedArea.yMax;
+            m_velocity.z = -m_velocity.z * m_bounciness;
+        }
+
+        transform.localPosition = newPosition;
     }
-
-    #endregion
-
-    #region 方法
-
-    #endregion
-
-    #region 事件
-
-    #endregion
-
-    #region 属性
 
     #endregion
 
@@ -61,18 +64,30 @@ public class MovingSphere : MonoBehaviour
     /// </summary>
     [SerializeField]
     [Range(0f, 100f)]
-    private float maxSpeed = 10f;
+    private float m_maxSpeed = 10f;
 
     /// <summary>
     /// 最大加速度
     /// </summary>
     [SerializeField, Range(0f, 100f)]
-    private float maxAcceleration = 10f;
+    private float m_maxAcceleration = 10f;
 
     /// <summary>
     ///  当前速度
     /// </summary>
-    private Vector3 velocity;
+    private Vector3 m_velocity;
+
+    /// <summary>
+    ///  限制移动的范围
+    /// </summary>
+    [SerializeField]
+    private Rect m_allowedArea = new Rect(-5f, -5f, 10f, 10f);
+
+    /// <summary>
+    ///  弹力
+    /// </summary>
+    [SerializeField, Range(0f, 1f)]
+    private float m_bounciness = 0.5f;
 
     #endregion
 }
